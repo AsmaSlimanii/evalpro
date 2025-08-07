fcb5a6f1a076ddf3fdd1e4444bb4eaf883bd56b1
AsmaSlimanii <asmaslimani@esprit.tn>
2025-08-07 14:02:58 +0200

    modification dans java

---
+++ EvalPro/src/test/java/com/medianet/evalpro/SimpleUnitTest.java
@@ -0,0 +1,13 @@
+package com.medianet.evalpro;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleUnitTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1); // ✅ Vérifie que 1 + 1 = 2
+    }
+}
